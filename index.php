7450b37307d4e7f3065ce8ff303badc5bf8bf87e
mkdesign <me@mkdesign.ir>
2014-04-07 15:56:50 +0430

    start to work

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8" />
+	<title>.::Bokeh::.</title>
+	<link rel="stylesheet" href="reset.css" />
+	<link rel="stylesheet" href="style.css" />
+	<script type="text/javascript" src="zepto.js"></script>
+	<script type="text/javascript" src="scripts.js"></script>
+</head>
+<body>
+	<div class="container">
+		
+	</div>
+</body>
+</html>
